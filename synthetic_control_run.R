7d704328b1e35f3db916336a16e78dfff55c9688
Daniel Weinberger <stopaction13@gmail.com>
2018-01-15 21:59:45 -0500

    Update synthetic_control_run.R

--- synthetic_control_run.R
+++ synthetic_control_run.R
@@ -8,7 +8,7 @@ gc()
 
 #Set the working directory
 #This step only works as written if this file is run using the source() command. Otherwise, skip this step and set manually.
-	setwd('C:\\Users\\dmw63\\Desktop\\My documents h\\GATES\\CausalImpact code\\Git-synthetic-control-master BoomSpikeSlab\\synthetic-control-master\\')
+	setwd('C:\\sc1\\')  #directory where .Rmd file is saved
 
 #Used to check for relevant packages and update them if out of date or install them if not installed.
 update_packages  <- TRUE #Whether to update outdated packages.
@@ -22,10 +22,10 @@ exclude_covar <- c()      #User-defined list of covariate columns to exclude fro
 exclude_group <- c()      #User-defined list of groups to exclude from analyses.
 code_change   <- TRUE     #Used for Brazil data. Set to TRUE to adjust for year 2008 coding changes; otherwise, set to FALSE.
 
-input_directory  <- 'https://raw.githubusercontent.com/weinbergerlab/synthetic-control/master/Datasets%20for%20PNAS/' #Directory (or URL) containing input data file.
-output_directory <- 'C:\\Users\\dmw63\\Desktop\\My documents h\\GATES\\CausalImpact code\\Git-synthetic-control-master BoomSpikeSlab\\synthetic-control-master\\Results'   #Directory where results will be saved.
+input_directory  <- 'C:\\sc1\\Datasets for PNAS\\' #Directory (or URL) containing input data file.
+output_directory <- 'C:\\sc1\\Results'   #Directory where results will be saved.
 output_directory <- paste(output_directory, format(Sys.time(), '%Y-%m-%d-%H%M%S'), '/', sep = '')                     #Adds a subfolder to output directory to organize results by date and time run.
-file_name        <- 'Dataset%20S1%20Brazil.csv'                                                                       #Name of file containing data for analysis. Must be a .csv file.
+file_name        <- 'Dataset S1% Brazil.csv'                                                                       #Name of file containing data for analysis. Must be a .csv file.
 
 group_name   <- 'age_group' #Name of column containing group labels.
 date_name    <- 'date'      #Name of column containing dates.
